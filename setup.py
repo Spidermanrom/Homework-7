b246b61bbdc223ea21c02d10a685c3f491cef301
Spidermanrom <93603589+Spidermanrom@users.noreply.github.com>
2023-09-19 19:35:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup,  find_packages
+from setuptools import setup, find_packages
+
 setup(
     name='clean_folder',
-    version='0.0.1',
-    description='Clean folder my desk',
-    author='Oleksandr Chonka',
-    license='MIT',
+    version='0.1',
     packages=find_packages(),
-    install_requires=['markdown'],
-    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main'] }
-)
+    entry_points={
+        'console_scripts': [
+            'clean-folder = clean_folder.clean:main'
+        ]
+    }
